f186646fc559076b82c081ba5ae8488cd07d541c
Carla Gomez <c.gomez@zoovu.com>
2023-04-27 11:36:54 -0400

    remove parametrization

--- tests/test_purchase.py
+++ tests/test_purchase.py
@@ -15,34 +15,6 @@ load_dotenv()
 login_name = os.getenv("LOGIN_NAME")
 login_password = os.getenv("LOGIN_PASSWORD")
 
-search_for_item_data = [
-    ("Fragrance", "Women", "Fragrance for Women"),
-]
-
-sort_by_data = [
-    ("p.price-ASC"),
-]
-
-item_name_data = [
-    ("ck One Gift Set"),
-]
-
-item_price_data = [
-    ("$36.00"),
-]
-
-product_data = [
-    ("ck One Gift Set", "$36.00", "$41.06"),
-]
-
-shipping_data = [
-    ("223", "3630", "76876"),
-]
-
-before_sorting_data = [
-    ("Flora By Gucci Eau Fraiche", "$90.00"),
-]
-
 # add docstring to all functions, modules and classes
 
 
@@ -55,131 +27,89 @@ def run_around_tests(
     authentication_page.login(login_name, login_password)
 
 
-@pytest.mark.parametrize("product, gender, product_category", search_for_item_data)
-def test_search_for_item(page: Page, product, gender, product_category) -> None:
+def test_search_for_item(page: Page) -> None:
     items_page = ItemsPage(page)
-    items_page.search_for_products(product, gender)
-    items_page.validate_category_name(product_category)
+    items_page.search_for_products("Fragrance", "Women")
+    items_page.validate_category_name("Fragrance for Women")
 
 
-@pytest.mark.parametrize("product, gender, product_category", search_for_item_data)
-@pytest.mark.parametrize("sort_criteria", sort_by_data)
-@pytest.mark.parametrize("item_name_before, item_price_before", before_sorting_data)
-def test_sort_by(
-    page: Page,
-    product,
-    gender,
-    product_category,
-    item_name_before,
-    item_price_before,
-    sort_criteria,
-) -> None:
+def test_sort_by(page: Page) -> None:
     items_page = ItemsPage(page)
-    items_page.search_for_products(product, gender)
-    items_page.validate_category_name(product_category)
+    items_page.search_for_products("Fragrance", "Women")
+    items_page.validate_category_name("Fragrance for Women")
     items_page.go_to_item_details()
-    items_page.validate_item_name(item_name_before)
-    items_page.validate_item_price(item_price_before)
+    items_page.validate_item_name("Flora By Gucci Eau Fraiche")
+    items_page.validate_item_price("$90.00")
     items_page.go_back_to_products()
-    items_page.sort_by(sort_criteria)
+    items_page.sort_by("p.price-ASC")
 
 
-@pytest.mark.parametrize("product, gender, product_category", search_for_item_data)
-@pytest.mark.parametrize("sort_criteria", sort_by_data)
-@pytest.mark.parametrize("item_name", item_name_data)
-@pytest.mark.parametrize("item_price", item_price_data)
-@pytest.mark.parametrize("item_name_before, item_price_before", before_sorting_data)
-def test_view_item_details(
-    page: Page,
-    product,
-    gender,
-    product_category,
-    item_name_before,
-    item_price_before,
-    sort_criteria,
-    item_name,
-    item_price,
-) -> None:
+def test_view_item_details(page: Page) -> None:
     items_page = ItemsPage(page)
-    items_page.search_for_products(product, gender)
-    items_page.validate_category_name(product_category)
+    items_page.search_for_products("Fragrance", "Women")
+    items_page.validate_category_name("Fragrance for Women")
     items_page.go_to_item_details()
-    items_page.validate_item_name(item_name_before)
-    items_page.validate_item_price(item_price_before)
+    items_page.validate_item_name("Flora By Gucci Eau Fraiche")
+    items_page.validate_item_price("$90.00")
     items_page.go_back_to_products()
-    items_page.sort_by(sort_criteria)
+    items_page.sort_by("p.price-ASC")
     items_page.go_to_item_details()
-    items_page.validate_item_name(item_name)
-    items_page.validate_item_price(item_price)
+    items_page.validate_item_name("ck One Gift Set")
+    items_page.validate_item_price("$36.00")
 
 
-@pytest.mark.parametrize("product, gender, product_category", search_for_item_data)
-@pytest.mark.parametrize("sort_criteria", sort_by_data)
-@pytest.mark.parametrize("item_name", item_name_data)
-@pytest.mark.parametrize("item_price", item_price_data)
-def test_add_item_to_cart(
-    page: Page, product, gender, product_category, sort_criteria, item_name, item_price
-) -> None:
+def test_add_item_to_cart(page: Page) -> None:
     items_page = ItemsPage(page)
     checkout_page = CheckoutPage(page)
-    items_page.search_for_products(product, gender)
-    items_page.validate_category_name(product_category)
-    items_page.sort_by(sort_criteria)
+    items_page.search_for_products("Fragrance", "Women")
+    items_page.validate_category_name("Fragrance for Women")
+    items_page.sort_by("p.price-ASC")
     items_page.go_to_item_details()
-    items_page.validate_item_name(item_name)
-    items_page.validate_item_price(item_price)
+    items_page.validate_item_name("ck One Gift Set")
+    items_page.validate_item_price("$36.00")
     checkout_page.add_item_to_cart("1")
-    checkout_page.validate_items_on_shopping_cart("Shopping Cart", item_name, "1")
-
-
-@pytest.mark.parametrize("item_name", item_name_data)
-def test_view_shopping_cart(page: Page, item_name) -> None:
-    checkout_page = CheckoutPage(page)
-    checkout_page.go_to_shopping_cart()
-    checkout_page.validate_items_on_shopping_cart("Shopping Cart", item_name, "1")
-
-
-@pytest.mark.parametrize("item_name, item_price, total_price", product_data)
-@pytest.mark.parametrize("country_id, region_id, zipcode", shipping_data)
-def test_fill_shipping_information(
-    page: Page, item_name, item_price, total_price, country_id, region_id, zipcode
-) -> None:
-    checkout_page = CheckoutPage(page)
-    checkout_page.go_to_shopping_cart()
-    checkout_page.validate_items_on_shopping_cart("Shopping Cart", item_name, 1)
-    checkout_page.fill_shipping(country_id, region_id, zipcode)
-    checkout_page.validate_shipping_info(item_price, total_price)
-
-
-@pytest.mark.parametrize("item_name, item_price, total_price", product_data)
-@pytest.mark.parametrize("country_id, region_id, zipcode", shipping_data)
-def test_checkout(
-    page: Page, item_name, item_price, total_price, country_id, region_id, zipcode
-) -> None:
-    checkout_page = CheckoutPage(page)
-    checkout_page.go_to_shopping_cart()
-    checkout_page.validate_items_on_shopping_cart("Shopping Cart", item_name, 1)
-    checkout_page.fill_shipping(country_id, region_id, zipcode)
-    checkout_page.validate_shipping_info(item_price, total_price)
-    checkout_page.checkout()
-    checkout_page.validate_checkout_info(
-        "Checkout Confirmation", item_name, item_price, total_price
+    checkout_page.validate_items_on_shopping_cart(
+        "Shopping Cart", "ck One Gift Set", "1"
     )
 
 
-@pytest.mark.parametrize("item_name, item_price, total_price", product_data)
-@pytest.mark.parametrize("country_id, region_id, zipcode", shipping_data)
-def test_confirm_purchase(
-    page: Page, item_name, item_price, total_price, country_id, region_id, zipcode
-) -> None:
+def test_view_shopping_cart(page: Page) -> None:
     checkout_page = CheckoutPage(page)
     checkout_page.go_to_shopping_cart()
-    checkout_page.validate_items_on_shopping_cart("Shopping Cart", item_name, 1)
-    checkout_page.fill_shipping(country_id, region_id, zipcode)
-    checkout_page.validate_shipping_info(item_price, total_price)
+    checkout_page.validate_items_on_shopping_cart(
+        "Shopping Cart", "ck One Gift Set", "1"
+    )
+
+
+def test_fill_shipping_information(page: Page) -> None:
+    checkout_page = CheckoutPage(page)
+    checkout_page.go_to_shopping_cart()
+    checkout_page.validate_items_on_shopping_cart("Shopping Cart", "ck One Gift Set", 1)
+    checkout_page.fill_shipping("223", "3630", "76876")
+    checkout_page.validate_shipping_info("$36.00", "$41.06")
+
+
+def test_checkout(page: Page) -> None:
+    checkout_page = CheckoutPage(page)
+    checkout_page.go_to_shopping_cart()
+    checkout_page.validate_items_on_shopping_cart("Shopping Cart", "ck One Gift Set", 1)
+    checkout_page.fill_shipping("223", "3630", "76876")
+    checkout_page.validate_shipping_info("$36.00", "$41.06")
     checkout_page.checkout()
     checkout_page.validate_checkout_info(
-        "Checkout Confirmation", item_name, item_price, total_price
+        "Checkout Confirmation", "ck One Gift Set", "$36.00", "$41.06"
+    )
+
+
+def test_confirm_purchase(page: Page) -> None:
+    checkout_page = CheckoutPage(page)
+    checkout_page.go_to_shopping_cart()
+    checkout_page.validate_items_on_shopping_cart("Shopping Cart", "ck One Gift Set", 1)
+    checkout_page.fill_shipping("223", "3630", "76876")
+    checkout_page.validate_shipping_info("$36.00", "$41.06")
+    checkout_page.checkout()
+    checkout_page.validate_checkout_info(
+        "Checkout Confirmation", "ck One Gift Set", "$36.00", "$41.06"
     )
     checkout_page.confirm_purchase()
     checkout_page.validate_purchase("Your Order Has Been Processed!")
